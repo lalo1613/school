ee270e397c1179566fd911aa85ef70ae9ead9ccf
chenbengal <Bengal311!>
2020-04-04 14:31:21 +0300

    testing inside epochs

--- DLcourse/ex1.py
+++ DLcourse/ex1.py
@@ -40,45 +40,72 @@ test_images = torch.tensor(test_images).float()
 test_labels = torch.tensor(test_labels)
 
 #  None
-acc_none, acc_none_test = Training_LENET(train_images = train_images, train_labels = train_labels,test_images = test_images, test_labels = test_labels, dir_input = dir_input ,NetName = Net_None,optimizer_input= None, n_epochs = 15)
-acc_l2, acc_l2_test = Training_LENET(train_images = train_images, train_labels = train_labels,test_images = test_images, test_labels = test_labels, dir_input = dir_input ,NetName = Net_None,optimizer_input= 'l2', n_epochs = 15)
+acc_none, acc_none_tst = Training_LENET(train_images = train_images, train_labels = train_labels,test_images = test_images, test_labels = test_labels, dir_input = dir_input ,NetName = Net_None,optimizer_input= None, n_epochs = 15)
+acc_l2, acc_l2_tst = Training_LENET(train_images = train_images, train_labels = train_labels,test_images = test_images, test_labels = test_labels, dir_input = dir_input ,NetName = Net_None,optimizer_input= 'l2', n_epochs = 15)
 
 # Dropout
-acc_dropout, acc_dropout_test = Training_LENET(train_images = train_images, train_labels = train_labels,test_images = test_images, test_labels = test_labels, dir_input = dir_input ,NetName = Net_Dropout,optimizer_input= None, n_epochs = 15)
-acc_dropout_l2,acc_dropout_l2_test = Training_LENET(train_images = train_images, train_labels = train_labels, test_images = test_images, test_labels = test_labels,dir_input = dir_input ,NetName = Net_Dropout,optimizer_input= 'l2', n_epochs = 15)
+acc_dropout, acc_dropout_tst = Training_LENET(train_images = train_images, train_labels = train_labels,test_images = test_images, test_labels = test_labels, dir_input = dir_input ,NetName = Net_Dropout,optimizer_input= None, n_epochs = 15)
+acc_dropout_l2,acc_dropout_l2_tst = Training_LENET(train_images = train_images, train_labels = train_labels, test_images = test_images, test_labels = test_labels,dir_input = dir_input ,NetName = Net_Dropout,optimizer_input= 'l2', n_epochs = 15)
 
 # BatchNorm
-acc_batch_norm,acc_batch_norm_test = Training_LENET(train_images = train_images, train_labels = train_labels,test_images = test_images, test_labels = test_labels, dir_input = dir_input ,NetName = Net_BatchNorm,optimizer_input= None, n_epochs = 15)
-acc_batch_norm_l2,acc_batch_norm_l2_test = Training_LENET(train_images = train_images, train_labels = train_labels,test_images = test_images, test_labels = test_labels, dir_input = dir_input ,NetName = Net_BatchNorm,optimizer_input= 'l2', n_epochs = 15)
+acc_batch_norm,acc_batch_norm_tst = Training_LENET(train_images = train_images, train_labels = train_labels,test_images = test_images, test_labels = test_labels, dir_input = dir_input ,NetName = Net_BatchNorm,optimizer_input= None, n_epochs = 15)
+acc_batch_norm_l2,acc_batch_norm_l2_tst = Training_LENET(train_images = train_images, train_labels = train_labels,test_images = test_images, test_labels = test_labels, dir_input = dir_input ,NetName = Net_BatchNorm,optimizer_input= 'l2', n_epochs = 15)
 
 # Dropout + BatchNorm
-acc_dropout_batch_norm,acc_dropout_batch_norm_test = Training_LENET(train_images = train_images, train_labels = train_labels,test_images = test_images, test_labels = test_labels, dir_input = dir_input ,NetName = Net_Dropout_BatchNorm,optimizer_input= None, n_epochs = 15)
-acc_dropout_batch_norm_l2,acc_dropout_batch_norm_l2_test = Training_LENET(train_images = train_images, train_labels = train_labels,test_images = test_images, test_labels = test_labels, dir_input = dir_input ,NetName = Net_Dropout_BatchNorm,optimizer_input= 'l2', n_epochs = 15)
+acc_dropout_batch_norm,acc_dropout_batch_norm_tst = Training_LENET(train_images = train_images, train_labels = train_labels,test_images = test_images, test_labels = test_labels, dir_input = dir_input ,NetName = Net_Dropout_BatchNorm,optimizer_input= None, n_epochs = 15)
+acc_dropout_batch_norm_l2,acc_dropout_batch_norm_l2_tst = Training_LENET(train_images = train_images, train_labels = train_labels,test_images = test_images, test_labels = test_labels, dir_input = dir_input ,NetName = Net_Dropout_BatchNorm,optimizer_input= 'l2', n_epochs = 15)
 
 
 # Plots
+
 plt.plot('epoch', 'accuracy', data=acc_none, color='blue', markersize=12, linewidth=4, label= 'None')
 plt.plot('epoch', 'accuracy', data=acc_l2, color='blue', markersize=12,linewidth=4, linestyle='dashed', label = 'L2')
-plt.plot('epoch', 'accuracy', data=acc_dropout, color='green', markersize=12, linewidth=4, label = 'Dropout')
-plt.plot('epoch', 'accuracy', data=acc_dropout_l2, color='green', markersize=12, linewidth=4, linestyle='dashed', label = 'Dropout + L2')
-plt.plot('epoch', 'accuracy', data=acc_dropout_batch_norm, color='red', markersize=12, linewidth=4, label = 'Dropout + BatchNorm')
-plt.plot('epoch', 'accuracy', data=acc_dropout_batch_norm_l2, color='red', markersize=12, linewidth=4, linestyle='dashed', label = 'Dropout + BatchNorm + L2')
-plt.plot('epoch', 'accuracy', data=acc_batch_norm, color='brown', markersize=12,linewidth=4, label = 'BatchNorm')
-plt.plot('epoch', 'accuracy', data=acc_batch_norm_l2, color='brown', markersize=12, linewidth=4, linestyle='dashed', label= 'BatchNorm + L2')
+plt.plot('epoch', 'accuracy', data=acc_none_tst, color='blue', markersize=12, linewidth=4, label= 'None')
+plt.plot('epoch', 'accuracy', data=acc_l2_tst, color='blue', markersize=12,linewidth=4, linestyle='dashed', label = 'L2')
 plt.xlabel('epochs')
 plt.ylabel('accuracy')
 plt.title('Training Accuracy Per Epoch')
 plt.legend()
 
 
-acc_none_tst = TestingNet(test_images = test_images, test_labels = test_labels, NetName = Net_None, dir_input = dir_input)
-acc_l2_tst = TestingNet(test_images = test_images, test_labels = test_labels, NetName = Net_None, dir_input = dir_input, optimizer_input = 'l2')
-acc_dropout_tst = TestingNet(test_images = test_images, test_labels = test_labels, NetName = Net_Dropout, dir_input = dir_input)
-acc_dropout_l2_tst = TestingNet(test_images = test_images, test_labels = test_labels, NetName = Net_Dropout, dir_input = dir_input, optimizer_input = 'l2')
-acc_dropout_batch_norm_tst = TestingNet(test_images = test_images, test_labels = test_labels, NetName = Net_Dropout_BatchNorm, dir_input = dir_input)
-acc_dropout_batch_norm_l2_tst = TestingNet(test_images = test_images, test_labels = test_labels, NetName = Net_Dropout_BatchNorm, dir_input = dir_input, optimizer_input = 'l2')
-acc_batch_norm_tst = TestingNet(test_images = test_images, test_labels = test_labels, NetName = Net_BatchNorm, dir_input = dir_input)
-acc_batch_norm_l2_tst = TestingNet(test_images = test_images, test_labels = test_labels, NetName = Net_BatchNorm, dir_input = dir_input, optimizer_input = 'l2')
+plt.plot('epoch', 'accuracy', data=acc_dropout, color='green', markersize=12, linewidth=4, label = 'Dropout')
+plt.plot('epoch', 'accuracy', data=acc_dropout_l2, color='green', markersize=12, linewidth=4, linestyle='dashed', label = 'Dropout + L2')
+plt.plot('epoch', 'accuracy', data=acc_dropout_tst, color='green', markersize=12, linewidth=4, label = 'Dropout')
+plt.plot('epoch', 'accuracy', data=acc_dropout_l2_tst, color='green', markersize=12, linewidth=4, linestyle='dashed', label = 'Dropout + L2')
+plt.xlabel('epochs')
+plt.ylabel('accuracy')
+plt.title('Training Accuracy Per Epoch')
+plt.legend()
+
+
+plt.plot('epoch', 'accuracy', data=acc_dropout_batch_norm, color='red', markersize=12, linewidth=4, label = 'Dropout + BatchNorm')
+plt.plot('epoch', 'accuracy', data=acc_dropout_batch_norm_l2, color='red', markersize=12, linewidth=4, linestyle='dashed', label = 'Dropout + BatchNorm + L2')
+plt.plot('epoch', 'accuracy', data=acc_dropout_batch_norm_tst, color='red', markersize=12, linewidth=4, label = 'Dropout + BatchNorm')
+plt.plot('epoch', 'accuracy', data=acc_dropout_batch_norm_l2_tst, color='red', markersize=12, linewidth=4, linestyle='dashed', label = 'Dropout + BatchNorm + L2')
+plt.xlabel('epochs')
+plt.ylabel('accuracy')
+plt.title('Training Accuracy Per Epoch')
+plt.legend()
+
+
+plt.plot('epoch', 'accuracy', data=acc_batch_norm, color='brown', markersize=12,linewidth=4, label = 'BatchNorm')
+plt.plot('epoch', 'accuracy', data=acc_batch_norm_l2, color='brown', markersize=12, linewidth=4, linestyle='dashed', label= 'BatchNorm + L2')
+plt.plot('epoch', 'accuracy', data=acc_batch_norm_tst, color='brown', markersize=12,linewidth=4, label = 'BatchNorm')
+plt.plot('epoch', 'accuracy', data=acc_batch_norm_l2_tst, color='brown', markersize=12, linewidth=4, linestyle='dashed', label= 'BatchNorm + L2')
+plt.xlabel('epochs')
+plt.ylabel('accuracy')
+plt.title('Training Accuracy Per Epoch')
+plt.legend()
+
+
+# acc_none_tst = TestingNet(test_images = test_images, test_labels = test_labels, NetName = Net_None, dir_input = dir_input)
+# acc_l2_tst = TestingNet(test_images = test_images, test_labels = test_labels, NetName = Net_None, dir_input = dir_input, optimizer_input = 'l2')
+# acc_dropout_tst = TestingNet(test_images = test_images, test_labels = test_labels, NetName = Net_Dropout, dir_input = dir_input)
+# acc_dropout_l2_tst = TestingNet(test_images = test_images, test_labels = test_labels, NetName = Net_Dropout, dir_input = dir_input, optimizer_input = 'l2')
+# acc_dropout_batch_norm_tst = TestingNet(test_images = test_images, test_labels = test_labels, NetName = Net_Dropout_BatchNorm, dir_input = dir_input)
+# acc_dropout_batch_norm_l2_tst = TestingNet(test_images = test_images, test_labels = test_labels, NetName = Net_Dropout_BatchNorm, dir_input = dir_input, optimizer_input = 'l2')
+# acc_batch_norm_tst = TestingNet(test_images = test_images, test_labels = test_labels, NetName = Net_BatchNorm, dir_input = dir_input)
+# acc_batch_norm_l2_tst = TestingNet(test_images = test_images, test_labels = test_labels, NetName = Net_BatchNorm, dir_input = dir_input, optimizer_input = 'l2')
 
 ###############################################################
 
