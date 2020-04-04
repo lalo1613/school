d8f84c47c8521c1a7bddef1d8bd8f46ad8ffa14e
omri_ <omri1613@gmail.com>
2020-04-04 13:36:49 +0300

    added import

--- DLcourse/ex1.py
+++ DLcourse/ex1.py
@@ -6,10 +6,9 @@ import pandas as pd
 from mnist import MNIST
 import numpy as np
 import torch
-from DLcourse.lenet5 import Net_BatchNorm
-from DLcourse.lenet5 import Net_Dropout
-from DLcourse.lenet5 import Net_Dropout_BatchNorm
-from DLcourse.lenet5 import Net_None
+from DLcourse.lenet5 import Net_BatchNorm, Net_Dropout, Net_Dropout_BatchNorm, Net_None
+from DLcourse.training import Training_LENET
+from DLcourse.testing import TestingNet
 import matplotlib.pyplot as plt
 
 
@@ -87,87 +86,3 @@ print('Test Accuracy Comparrison: \n'
       'Dropout +  BatchNorm + L2 : {5}, \n'
       'BatchNorm : {6}, BatchNorm + L2 : {7}'.format(acc_none_tst,acc_l2_tst,acc_dropout_tst,acc_dropout_l2_tst,acc_dropout_batch_norm_tst,acc_dropout_batch_norm_l2_tst,acc_batch_norm_tst,acc_batch_norm_l2_tst))
 
-# training net
-# for epoch in range(10):  # loop over the dataset multiple times
-#     running_loss = 0.0
-#     batch_size = 1000
-#     for i in range(train_images.shape[0]//batch_size):
-#         # get the inputs; data is a list of [inputs, labels]
-#         inputs = train_images[(batch_size*i):(batch_size*(i+1))].apply_(float)
-#         labels = train_labels[(batch_size*i):(batch_size*(i+1))]
-#         inputs = inputs.reshape((batch_size,1,train_images.shape[1],train_images.shape[1]))
-#
-#
-# # loading CNN (net's class defined in separate script)
-# net = Net_None()
-#
-# # defining a Loss function and parameter optimizer
-# criterion = torch.nn.CrossEntropyLoss()
-# optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-# optimizer_l2 = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9 , weight_decay=0.01)
-#
-#
-# # training net
-# for epoch in range(10):  # loop over the dataset multiple times
-#     running_loss = 0.0
-#     batch_size = 1000
-#     for i in range(train_images.shape[0]//batch_size):
-#         # get the inputs; data is a list of [inputs, labels]
-#         inputs = train_images[(batch_size*i):(batch_size*(i+1))].apply_(float)
-#         labels = train_labels[(batch_size*i):(batch_size*(i+1))]
-#         inputs = inputs.reshape((batch_size,1,train_images.shape[1],train_images.shape[1]))
-#
-#         # zero the parameter gradients
-#         optimizer.zero_grad()
-#
-#         # forward + backward + optimize
-#         outputs = net(inputs)
-#         loss = criterion(outputs, labels)
-#         loss.backward()
-#         optimizer.step()
-#
-#         # print statistics
-#         running_loss += loss.item()
-#         print("loss "+str(running_loss/(i+1)))
-#
-# print('Finished Training')
-#
-# torch.save(net.state_dict(), dir_input+"outputs/model.pth")
-
-# Testing
-#
-# net = Net_None()
-# net.load_state_dict(torch.load(dir_input+"outputs/model.pth"))
-# test_images = test_images.reshape((test_images.shape[0],1,test_images.shape[1],test_images.shape[1]))
-# outputs = net(test_images)
-# values, indices = torch.max(outputs, 1)
-# preds = np.array(indices)
-# test_labels = np.array(test_labels)
-# acc_none = np.mean(preds == test_labels)
-#
-# net = Net_BatchNorm()
-# net.load_state_dict(torch.load(dir_input+"outputs/model.pth"))
-# test_images = test_images.reshape((test_images.shape[0],1,test_images.shape[1],test_images.shape[1]))
-# outputs = net(test_images)
-# values, indices = torch.max(outputs, 1)
-# preds = np.array(indices)
-# test_labels = np.array(test_labels)
-# acc_BatchNorm = np.mean(preds == test_labels)
-#
-# net = Net_Dropout()
-# net.load_state_dict(torch.load(dir_input+"outputs/model.pth"))
-# test_images = test_images.reshape((test_images.shape[0],1,test_images.shape[1],test_images.shape[1]))
-# outputs = net(test_images)
-# values, indices = torch.max(outputs, 1)
-# preds = np.array(indices)
-# test_labels = np.array(test_labels)
-# acc_Dropout = np.mean(preds == test_labels)
-#
-# net = Net_Dropout_BatchNorm()
-# net.load_state_dict(torch.load(dir_input+"outputs/model.pth"))
-# test_images = test_images.reshape((test_images.shape[0],1,test_images.shape[1],test_images.shape[1]))
-# outputs = net(test_images)
-# values, indices = torch.max(outputs, 1)
-# preds = np.array(indices)
-# test_labels = np.array(test_labels)
-# acc_Dropout_BatchNorm = np.mean(preds == test_labels)
