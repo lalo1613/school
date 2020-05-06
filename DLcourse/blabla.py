e819a77f84088749ffb14778344d78623e568896
chenbengal <Bengal311!>
2020-05-06 16:01:25 +0300

    works good for their data.

--- DLcourse/blabla.py
+++ DLcourse/blabla.py
@@ -2,32 +2,30 @@ import sys
 
 import chainer as chainer
 import torch
+import pandas as pd
+import matplotlib.pyplot as plt
 
 ########################################################################################################################
 # Basic RNN language model
 ########################################################################################################################
 
-class BasicRNNLM(torch.nn.Module):
+class GRU(torch.nn.Module):
 
-    def __init__(self,vocabulary_size,with_drops,method):
-        super(BasicRNNLM,self).__init__()
+    def __init__(self,vocabulary_size,with_drops):
+        super(GRU,self).__init__()
 
         # Configuration of our model
         self.num_layers=2
         embedding_size=200
         self.hidden_size=200
-        dropout_prob=0.5
+        dropout_prob=0.25
         self.with_drops = with_drops
-        self.method = method
         # Define embedding layer
         self.embed=torch.nn.Embedding(vocabulary_size,embedding_size)
 
-        # Define LSTM
-        self.lstm=torch.nn.LSTM(embedding_size,self.hidden_size,self.num_layers,dropout=dropout_prob,batch_first=True)
-
         # Define GRU
-        self.gru=torch.nn.GRU(embedding_size,self.hidden_size,self.num_layers,dropout=dropout_prob,batch_first=True)
-
+        self.gru=torch.nn.GRU(embedding_size,self.hidden_size,self.num_layers,batch_first=True)
+        self.gru_drop=torch.nn.GRU(embedding_size,self.hidden_size,self.num_layers,dropout=dropout_prob,batch_first=True)
 
         # Define dropout
         self.drop=torch.nn.Dropout(dropout_prob)
@@ -49,12 +47,70 @@ class BasicRNNLM(torch.nn.Module):
         # Run LSTM
         if self.with_drops == True:
             y=self.drop(y)
-        if self.method == 'LSTM':
-            y,h=self.lstm(y,h)
+            y,h=self.gru_drop(y,h)
+            y = self.drop(y)
         else:
             y,h=self.gru(y,h)
+
+        # Reshape
+        y=y.contiguous().reshape(-1,self.hidden_size)
+        # Fully-connected (decoding)
+        y=self.fc(y)
+        # Return prediction and states
+        return y,h
+
+    def get_initial_states(self,batch_size):
+        # Set initial hidden and memory states to 0
+        return (torch.autograd.Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size)) )
+             #,torch.autograd.Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size)) )
+
+    def detach(self,h):
+        # Detach returns a new variable, decoupled from the current computation graph
+        h[0].detach() #,h[1].detach()
+
+
+class LSTM(torch.nn.Module):
+
+    def __init__(self,vocabulary_size,with_drops):
+        super(LSTM,self).__init__()
+
+        # Configuration of our model
+        self.num_layers=2
+        embedding_size=200
+        self.hidden_size=200
+        dropout_prob=0.25
+        self.with_drops = with_drops
+        # Define embedding layer
+        self.embed=torch.nn.Embedding(vocabulary_size,embedding_size)
+
+        # Define LSTM
+        self.lstm=torch.nn.LSTM(embedding_size,self.hidden_size,self.num_layers,batch_first=True)
+        self.lstm_drop=torch.nn.LSTM(embedding_size,self.hidden_size,self.num_layers,dropout=dropout_prob,batch_first=True)
+
+        # Define dropout
+        self.drop=torch.nn.Dropout(dropout_prob)
+
+        # Define output layer
+        self.fc=torch.nn.Linear(self.hidden_size,vocabulary_size)
+
+        # Init weights
+        init_range=0.1
+        self.embed.weight.data.uniform_(-init_range,init_range)
+        self.fc.bias.data.fill_(0)
+        self.fc.weight.data.uniform_(-init_range,init_range)
+
+        return
+
+    def forward(self,x,h):
+        # Apply embedding (encoding)
+        y=self.embed(x)
+        # Run LSTM
         if self.with_drops == True:
             y=self.drop(y)
+            y,h=self.lstm_drop(y,h)
+        else:
+            y,h=self.lstm(y,h)
+
         # Reshape
         y=y.contiguous().reshape(-1,self.hidden_size)
         # Fully-connected (decoding)
@@ -65,7 +121,7 @@ class BasicRNNLM(torch.nn.Module):
     def get_initial_states(self,batch_size):
         # Set initial hidden and memory states to 0
         return (torch.autograd.Variable(torch.zeros(self.num_layers,batch_size,self.hidden_size))#.cuda(),
-                ,torch.autograd.Variable(torch.zeros(self.num_layers,batch_size,self.hidden_size)) )#.cuda())
+         ,torch.autograd.Variable(torch.zeros(self.num_layers,batch_size,self.hidden_size)) )#.cuda())
 
     def detach(self,h):
         # Detach returns a new variable, decoupled from the current computation graph
@@ -73,6 +129,8 @@ class BasicRNNLM(torch.nn.Module):
 
 
 
+
+###########################################################
 import sys,argparse
 import numpy as np
 import  pickle
@@ -122,7 +180,7 @@ data_valid=torch.LongTensor(val.astype(np.int64))
 data_test=torch.LongTensor(test.astype(np.int64))
 
 # Make batches
-batch_size = 20
+batch_size = 256
 num_batches=data_train.size(0)//batch_size         # Get number of batches
 data_train=data_train[:num_batches*batch_size]     # Trim last elements
 data_train=data_train.view(batch_size,-1)          # Reshape
@@ -140,10 +198,12 @@ data_test=data_test.view(batch_size,-1)
 print ('Init...')
 learning_rate = 0.1
 # Instantiate and init the model, and move it to the GPU
-model_LSTM  =BasicRNNLM(vocabulary_size=vocabulary_size,with_drops=False,method='LSTM')#.cuda()
-model_LSTM_drop  =BasicRNNLM(vocabulary_size=vocabulary_size,with_drops=True,method='LSTM')#.cuda()
-model_GRU  =BasicRNNLM(vocabulary_size=vocabulary_size,with_drops=False,method='GRU')#.cuda()
-model_GRU_drop  =BasicRNNLM(vocabulary_size=vocabulary_size,with_drops=True,method='GRU')#.cuda()
+model_LSTM  =LSTM(vocabulary_size=vocabulary_size,with_drops=False)#.cuda()
+model_LSTM_drop  =LSTM(vocabulary_size=vocabulary_size,with_drops=True)#.cuda()
+
+model_GRU  =GRU(vocabulary_size=vocabulary_size,with_drops=False)#.cuda()
+model_GRU_drop =GRU(vocabulary_size=vocabulary_size,with_drops=True)#.cuda()
+
 
 # Define loss function
 criterion=torch.nn.CrossEntropyLoss(size_average=False)
@@ -156,7 +216,7 @@ optimizer=torch.optim.SGD(model_LSTM.parameters(),lr=learning_rate)
 ########################################################################################################################
 # Train/test routines
 ########################################################################################################################
-bptt = 35
+bptt = 100
 def train(data,model,criterion,optimizer):
     clip_norm = 0.25
     # Set model to training mode (we're using dropout)
@@ -224,9 +284,9 @@ def eval(data,model,criterion):
 # Train/validation/test
 ########################################################################################################################
 
-def run_dataset_in_net(model_input,learning_rate = learning_rate, data_train = data_train, data_valid = data_valid, data_test = data_test, optimizer = optimizer):
+def run_dataset_in_net(model_input,file_name,num_epochs,dir_input =dir_input, learning_rate = learning_rate, data_train = data_train, data_valid = data_valid, data_test = data_test, optimizer = optimizer):
     print('Train...')
-    num_epochs = 5
+    num_epochs = num_epochs
     anneal_factor = 2.0
     # Loop training epochs
     lr=learning_rate
@@ -239,6 +299,7 @@ def run_dataset_in_net(model_input,learning_rate = learning_rate, data_train = d
         # Train
         model =train(data_train,model_input,criterion,optimizer)
         train_loss = eval(data_train, model, criterion)
+
 
         # Validation
         val_loss=eval(data_valid,model,criterion)
@@ -259,14 +320,40 @@ def run_dataset_in_net(model_input,learning_rate = learning_rate, data_train = d
         perplexity_val.append(np.exp(val_loss))
         perplexity_test.append(np.exp(test_loss))
         tqdm.write(msg)
+    DF = pd.DataFrame({'Train':perplexity_train,'Validation':perplexity_val,'Test':perplexity_test})
+    DF.to_csv(dir_input+ "outputs/" + file_name + ".csv")
+    torch.save(model.state_dict(), dir_input + "outputs/" + file_name + ".pth")
+    return DF
 
-    return perplexity_train,perplexity_val, perplexity_test
+
+LSTM = run_dataset_in_net(model_input = model_LSTM,file_name = 'model_LSTM', num_epochs = 15)#,learning_rate = learning_rate, data_train = data_train, data_valid = data_valid, data_test = data_test, optimizer = optimizer):
+LSTM_drop = run_dataset_in_net(model_input = model_LSTM_drop,file_name = 'model_LSTM_drop', num_epochs = 1)#,learning_rate = learning_rate, data_train = data_train, data_valid = data_valid, data_test = data_test, optimizer = optimizer):
+GRU = run_dataset_in_net(model_input = model_GRU,file_name = 'model_GRU', num_epochs = 1)#,learning_rate = learning_rate, data_train = data_train, data_valid = data_valid, data_test = data_test, optimizer = optimizer):
+GRU_drop = run_dataset_in_net(model_input = model_GRU_drop,file_name = 'model_GRU_drop', num_epochs = 15)#,learning_rate = learning_rate, data_train = data_train, data_valid = data_valid, data_test = data_test, optimizer = optimizer):
+
+#   Upload results without running the models
+#####################################################
+LSTM = pd.read_csv(dir_input+ "outputs/" + "model_LSTM" + ".csv")[['Train','Validation','Test']]
+LSTM_drop = pd.read_csv(dir_input+ "outputs/" + "model_LSTM_drop" + ".csv")[['Train','Validation','Test']]
+GRU = pd.read_csv(dir_input+ "outputs/" + "model_GRU" + ".csv")[['Train','Validation','Test']]
+GRU_drop = pd.read_csv(dir_input+ "outputs/" + "model_GRU_drop" + ".csv")[['Train','Validation','Test']]
+#####################################################
+
+trains = pd.DataFrame({'LSTM':LSTM['Train'],'LSTM_Dropout':LSTM_drop['Train'],'GRU':GRU['Train'],'GRU_Dropout':GRU_drop['Train']})
+vals = pd.DataFrame({'LSTM':LSTM['Validation'],'LSTM_Dropout':LSTM_drop['Validation'],'GRU':GRU['Validation'],'GRU_Dropout':GRU_drop['Validation']})
+tests = pd.DataFrame({'LSTM':LSTM['Test'],'LSTM_Dropout':LSTM_drop['Test'],'GRU':GRU['Test'],'GRU_Dropout':GRU_drop['Test']})
 
 
-train, val, test = run_dataset_in_net(model_input = model_LSTM)#,learning_rate = learning_rate, data_train = data_train, data_valid = data_valid, data_test = data_test, optimizer = optimizer):
-train
-val
-test
+LSTM.plot(lw=2, colormap='jet', marker='.', markersize=10, title='LSTM perplexity')
+LSTM_drop.plot(lw=2, colormap='jet', marker='.', markersize=10, title='LSTM with Dropouts perplexity')
+GRU.plot(lw=2, colormap='jet', marker='.', markersize=10, title='GRU perplexity')
+GRU_drop.plot(lw=2, colormap='jet', marker='.', markersize=10, title='GRU with Dropouts perplexity')
+
+trains.plot(lw=2, colormap='jet', marker='.', markersize=10, title='Train dataset perplexity')
+vals.plot(lw=2, colormap='jet', marker='.', markersize=10, title='Validation dataset perplexity')
+tests.plot(lw=2, colormap='jet', marker='.', markersize=10, title='Test dataset perplexity')
+
+# צריך להבין למה לוקח כל כך הרבה זמן לרוץ ואיך לשמור אוטומטית את השמות של הקבצים
 
 """
 ########################################################
