8cfc9d98b2923a766c68bcc3142a82635e84b960
omri_ <omri1613@gmail.com>
2020-10-08 16:22:20 +0300

    project commit

---
+++ DLcourse/preprocessing.py
@@ -0,0 +1,116 @@
+import numpy as np
+import pandas as pd
+import cv2
+import pickle
+import os
+import re
+from tqdm import tqdm
+import torch
+import face_recognition
+from PIL import Image
+
+
+def getFrame(vidcap, sec, count, path):
+    # func that captures images and saves them
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+    hasFrames, image = vidcap.read()
+    if hasFrames:
+        cv2.imwrite(path+"\image"+str(count)+".jpg", image)
+    return hasFrames
+
+
+def pre_process_dataset(input_path, set_str):
+
+    if set_str + "_torch.pkl" in os.listdir(input_path):
+        with open(input_path + set_str + "_torch.pkl", "rb") as file:
+            load_dict = pickle.load(file)
+
+        dataset, dataset_labels = load_dict["dataset"], load_dict["dataset_labels"]
+        return dataset, dataset_labels
+
+    metadata = pd.read_json(input_path+'metadata.json').T
+
+    # creating images from video at constant intervals
+    all_video_names = [re.sub(".mp4","",img) for img in os.listdir(input_path) if re.findall(".mp4",img)]
+
+    sec = 0
+    frameRate = 0.5  # //it will capture image in each 0.5 second
+    count = 1
+
+    for video_name in tqdm(all_video_names):
+        VIDEO_STREAM = input_path+video_name+".mp4"
+        vidcap = cv2.VideoCapture(VIDEO_STREAM)
+
+        images_path = input_path+"images_"+video_name
+        os.makedirs(images_path, exist_ok=True)
+
+        success = getFrame(vidcap, sec, count, images_path)
+        while success:
+            count = count + 1
+            sec = sec + frameRate
+            sec = round(sec, 2)
+            success = getFrame(vidcap, sec, count, images_path)
+
+    # capturing faces within the images
+    for video_name in tqdm(all_video_names):
+        count = 1
+        if "face_images_" + video_name not in os.listdir(input_path):
+            for i in range(1, 21):
+                images_path = input_path + "images_" + video_name + "/image"+str(i)+".jpg"
+                src = cv2.imread(images_path)
+                if src is not None:
+                    image = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
+                    face_locations = face_recognition.face_locations(image)
+
+                    face_images_path = input_path + "face_images_" + video_name
+                    os.makedirs(face_images_path, exist_ok=True)
+
+                    for face_location in face_locations:
+                        top, right, bottom, left = face_location
+                        face_image = image[top:bottom, left:right]
+                        # plt.imshow(face_image)
+                        cv2.imwrite(face_images_path + r"\face_image" + str(count) + ".jpg", face_image)
+                        count += 1
+
+    # resizing images to 256x256
+    size = 32, 32
+    resized_output_path = input_path + "new_all_"+set_str+"_imgs/"
+    os.makedirs(resized_output_path, exist_ok=True)
+    for video_name in tqdm(all_video_names):
+        face_images_path = input_path + "face_images_" + video_name
+        for face_img in os.listdir(face_images_path):
+            im = Image.open(face_images_path+"/"+face_img)
+            im = im.resize(size, Image.ANTIALIAS)
+            im_num = re.findall('[0-9]+', face_img)[0]
+            im.save(resized_output_path+video_name+im_num+".jpg", "JPEG")
+
+    set_images = os.listdir(resized_output_path)
+    set_labels = [metadata.loc[vid+".mp4"]['label'] for vid in [re.sub('[0-9]+.jpg','',item) for item in os.listdir(resized_output_path)]]
+    set_labels_df = pd.DataFrame(zip(set_images, set_labels), columns=["image", "label"])
+
+    dataset = []
+    for img in tqdm(resized_output_path+set_labels_df["image"]):
+        img = cv2.imread(img)
+        img = np.moveaxis(img, 2, 0)
+        dataset.append(img)
+    dataset = np.array(dataset)
+
+    # np.array(dataset).shape   # (7442,256,256)
+    dataset = torch.tensor(dataset).float()
+    dataset_labels = (set_labels_df["label"] == "REAL").apply(int)
+    dataset_labels = torch.tensor(dataset_labels)
+
+    with open(input_path+set_str+"_torch.pkl","wb") as file:
+        pickle.dump({"dataset":dataset, "dataset_labels": dataset_labels}, file)
+
+    return dataset, dataset_labels
+
+
+# # loading test_input data
+# train_input_path = r"C:\Users\omri_\Downloads\train_videos/"
+# test_input_path = r"C:\Users\omri_\Downloads\train_sample_videos/"
+# # test_input_path = r"C:\Users\Bengal\Desktop\project/"
+#
+# train_set, train_labels = pre_process_dataset(train_input_path, "train")
+# test_set, test_labels = pre_process_dataset(test_input_path, "test")
+
---
+++ vgg_folder/main.py
@@ -0,0 +1,225 @@
+import torch
+import time
+import os
+import math
+from vgg_folder import vgg
+from tqdm import tqdm
+from DLcourse.preprocessing import pre_process_dataset
+
+save_dir = r"C:\Users\omri_\Downloads\train_videos\saving_dir/"
+model_names = 'vgg19'
+batch_size = 128
+workers = 4
+best_prec1 = 0
+momentum = 0.9
+lr = 0.05
+weight_decay = 5e-4
+start_epoch = 0
+epochs = 1
+print_freq = 20
+
+
+def train(train_loader, model, criterion, optimizer, epoch):
+    """
+        Run one train epoch
+    """
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+
+    # switch to train mode
+    model.train()
+    end = time.time()
+
+    for i, (input, target) in tqdm(enumerate(train_loader)):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        # compute output
+        output = model(input)
+        loss = criterion(output, target)
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        output = output.float()
+        loss = loss.float()
+
+        # measure accuracy and record loss
+        prec1 = accuracy(output.data, target)[0]
+        losses.update(loss.item(), input.size(0))
+        top1.update(prec1.item(), input.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
+                      epoch, i, len(train_loader), batch_time=batch_time,
+                      data_time=data_time, loss=losses, top1=top1))
+
+
+def validate(val_loader, model, criterion):
+    """
+    Run evaluation
+    """
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    end = time.time()
+    for i, (input, target) in enumerate(val_loader):
+        # compute output
+        with torch.no_grad():
+            output = model(input)
+            loss = criterion(output, target)
+
+        output = output.float()
+        loss = loss.float()
+
+        # measure accuracy and record loss
+        prec1 = accuracy(output.data, target)[0]
+        losses.update(loss.item(), input.size(0))
+        top1.update(prec1.item(), input.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % print_freq == 0:
+            print('Test: [{0}/{1}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
+                      i, len(val_loader), batch_time=batch_time, loss=losses,
+                      top1=top1))
+
+    print(' * Prec@1 {top1.avg:.3f}'
+          .format(top1=top1))
+
+    return top1.avg
+
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    """
+    Save the training model
+    """
+    torch.save(state, filename)
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def adjust_learning_rate(optimizer, epoch, lr):
+    """Sets the learning rate to the initial LR decayed by 2 every 30 epochs"""
+    lr = lr * (0.5 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+
+def our_loader(dataset, dataset_labels):
+    image_torch_list = []
+    label_torch_list = []
+    for i in range(math.ceil(dataset.shape[0]//batch_size)):
+        image_torch_list.append(dataset[(128*i):((128*(i+1)))])
+        label_torch_list.append(dataset_labels[(128*i):((128*(i+1)))])
+
+    return list(zip(image_torch_list, label_torch_list))
+
+
+def main():
+    # Check the save_dir exists or not
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    model = vgg.__dict__['vgg19']()
+
+    model.features = torch.nn.DataParallel(model.features)
+
+    # upload our data
+    print("Uploading Data")
+    train_input_path = r"C:\Users\omri_\Downloads\train_videos/"
+    test_input_path = r"C:\Users\omri_\Downloads\train_sample_videos/"
+
+    train_dataset, train_dataset_labels = pre_process_dataset(train_input_path, "train")
+    test_dataset, test_dataset_labels = pre_process_dataset(test_input_path, "test")
+
+    train_loader = our_loader(train_dataset, train_dataset_labels)
+    test_loader = our_loader(test_dataset, test_dataset_labels)
+
+    # todo: delete
+    train_loader = train_loader[:5]
+    test_loader = test_loader[:2]
+
+    # define loss function (criterion) and optimizer
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr,
+                                momentum=momentum,
+                                weight_decay=weight_decay)
+
+    print("Start Training")
+    for epoch in tqdm(range(start_epoch, epochs)):
+
+        # adjusting L.R
+        adjust_learning_rate(optimizer, epoch, lr)
+
+        # training model
+        train(train_loader, model, criterion, optimizer, epoch)
+
+        # evaluate on validation set
+        prec1 = validate(test_loader, model, criterion)
+
+        # remember best prec@1 and save checkpoint
+        is_best = prec1 > best_prec1
+        best_prec1 = max(prec1, best_prec1)
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            'best_prec1': best_prec1,
+        }, is_best, filename=os.path.join(save_dir, 'checkpoint_{}.tar'.format(epoch)))
+
+
+if __name__ == '__main__':
+    main()
